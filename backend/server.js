54edbfeb78f05e7a5fa97a1cc409125b650c8517
dzulfahmi <fitradzulfahmi@gmail.com>
2022-01-04 15:26:53 +0700

    check 4

--- backend/server.js
+++ backend/server.js
@@ -36,12 +36,22 @@ if (ENV === 'staging' || ENV === 'development') {
 
 const __dirname = path.resolve()
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '/frontend/build')))
-  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')))
-} else {
-  app.get('/', (req, res) => {
-    res.send('API is running...')
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static(path.join(__dirname, '/frontend/build')))
+//   app.get('*', (req, res) => res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html')))
+//   // app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')))
+// } else {
+//   app.get('/', (req, res) => {
+//     res.send('API is running...')
+//   })
+// }
+
+if (ENV === 'production' || ENV === 'staging') {
+  app.use((req, res, next) => {
+      if (req.header('x-forwarded-proto') !== 'https')
+          res.redirect(`https://${req.header('host')}${req.url}`)
+      else
+          next()
   })
 }
 
