343029f8c30cc31021cf363fe966c422cab70861
dzulfahmi <fitradzulfahmi@gmail.com>
2022-01-05 07:40:50 +0700

    improve code

--- backend/server.js
+++ backend/server.js
@@ -38,7 +38,10 @@ const __dirname = path.resolve()
 if (ENV === 'production') {
   // app.use(express.static(path.join(__dirname, '/frontend/build')))
   app.use(express.static('frontend/build'));
-  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')))
+  // app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')))
+  app.get("/*", function (req, res) {
+    res.sendFile(path.join(__dirname, "./frontend/build/index.html"));
+  });
   // app.get('*', (req, res) => res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html')))
 } else {
   app.get('/', (req, res) => {
